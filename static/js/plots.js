73080305b7c2f340e1c04f587c13ea8ae58296dc
nmrodio <nmrodio35@gmail.com>
2024-05-23 23:17:37 -0400

    Working code for all three visualizations

--- static/js/plots.js
+++ static/js/plots.js
@@ -84,63 +84,77 @@
 //     Plotly.newPlot('bar', [barChartData], barChartLayout);
 //   });
 
-// Car constructor names
+
+// Car constructor names from API end point data
 var carNames = ["Ferrari", "McLaren", "Mercedes", "Red Bull", "Williams"];
 
-        function createLineChart(carName) { 
-        d3.json('http://127.0.0.1:5000/car_speeds_data').then(carData => {
+// Function to pull in data from API end point
+function createLineChart(carName) {
+  d3.json('http://127.0.0.1:5000/car_speeds_data')
+    .then(carData => {
+      console.log(carData);
 
-        // Prepare line chart data
-        var selectedData = [];
+      // Array to hold line chart data
+      var selectedData = [];
 
-        carData.forEach(dataPoint => {
-            var year = dataPoint[0];
-            var constructor = dataPoint[1];
-            var points = dataPoint[2];
+      // Retreiving the necessary data from the API end point and saving the data into variables for the line chart
+      carData.forEach(dataPoint => {
+        var year = dataPoint[0];
+        var constructor = dataPoint[1];
+        var points = dataPoint[2];
 
-            if (constructor === carName) {
-            selectedData.push({ x: year, y: points });
-            }
-        });
-
-        // Create line chart trace (single car)
-        var lineTrace = {
-            x: selectedData.map(point => point.x),
-            y: selectedData.map(point => point.y),
-            name: carName,
-            mode: 'lines',
-            marker: { color: 'blue' } // Adjust color as desired
-        };
-
-        // Create a line chart layout
-        var lineChartLayout = {
-            title: `<b>${carName} Fastest Lap Speeds (2014-2024)</b>`,
-            xaxis: { title: `<b>Year<b>` },
-            yaxis: { title: `<b>Fastest Lap Speeds (km/hr)<b>` }
-        };
-
-        // Create the line chart
-        Plotly.newPlot('lineChart', [lineTrace], lineChartLayout);
-        })
-    }
-
-    function populateCarSelection() {
-    var carSelect = document.getElementById('carSelect');
-
-    carNames.forEach(carName => {
-        var option = document.createElement('option');
-        option.value = carName;
-        option.text = carName;
-        carSelect.add(option);
-    });
-
-    carSelect.addEventListener('change', function() {
-        var selectedCar = carSelect.value;
-        if (selectedCar === 'All') {
-        } else {
-        createLineChart(selectedCar);
+        if (constructor === carName) {
+          selectedData.push({ x: year, y: points });
         }
-    });
-    }
+      });
 
-    populateCarSelection();
+      // Create line chart trace (single car)
+      var lineTrace = {
+        x: selectedData.map(point => point.x),
+        y: selectedData.map(point => point.y),
+        name: carName,
+        mode: 'lines',
+        marker: { color: 'red' } // Adjust color as desired
+      };
+
+      // Creating a line chart layout
+      var lineChartLayout = {
+        title: `<b>${carName} Fastest Lap Speeds (2014-2024)</b>`,
+        xaxis: { title: '<b>Year</b>' },
+        yaxis: { title: '<b>Fastest Lap Speeds (km/hr)</b>' }
+      };
+
+      // Creating the line chart
+      Plotly.newPlot('lineChart', [lineTrace], lineChartLayout);
+    })
+    .catch(error => console.error('Error fetching data:', error));
+}
+
+function populateCarSelection() {
+  var carSelect = document.getElementById('carSelect');;
+
+  // Dynamically creating an 'option' element for the dropdown menu
+  carNames.forEach(carName => {
+    var option = document.createElement('option');
+    option.value = carName;
+    option.text = carName;
+    carSelect.add(option);
+  });
+
+  // Calling createLineChart to have "Ferrari" data as the default
+  createLineChart("Ferrari");
+
+  // Changes line chart to display data of selected car
+  carSelect.addEventListener('change', function() {
+    var selectedCar = carSelect.value;
+    if (selectedCar === 'All') {
+      fetchAllCarData()
+        .then(carData => plotAllCars(carData))
+        .catch(error => console.error('Error fetching data:', error));
+    } else {
+      createLineChart(selectedCar);
+    }
+  });
+}
+
+populateCarSelection();
